4faa4eeefdce5a0b65fcbf69fdf522198e0cec6d
kuzkinmax9-rgb <kuzkinmax9@gmail.com>
2025-10-01 12:12:27 +0300

    выполнили слияние 1

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ class Rectangle:
 
     def area(self):
         return self._a * self._b
+
     def get_height(self):
         return self._a
     
     def get_width(self):
         return self._b
 
-    def set_height(self, a):
-        self._a = a
+def set_height(self, a):
+    self._a = a
 
-    def set_width(self, b):
-        self._b = b
+def set_width(self, b):
+    self._b = b
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
 main_rect.set_height(7)
 main_rect.set_width(8)
+
