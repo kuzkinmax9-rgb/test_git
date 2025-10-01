9880ed8cedebe34cb81870625638b13e66d278e2
kuzkinmax9-rgb <kuzkinmax9@gmail.com>
2025-10-01 12:13:52 +0300

    выполнили слияние 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import pi
+
 class Rectangle:
     def __init__(self, a, b):
         self._a = a
@@ -18,8 +20,18 @@ def set_height(self, a):
 def set_width(self, b):
     self._b = b
 
+class Circle:
+    def __init__(self, r):
+        self._r = r
+
+    def area(self):
+        return pi * (self._r ** 2)
+
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
 main_rect.set_height(7)
 main_rect.set_width(8)
+main_circle = Circle(2)
+print(main_circle.area())
+
 
